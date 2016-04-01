34c7482d56d2204bee9582f6a8aa074cbab35167
Trine Aure <trine.aure@hotmail.com>
2016-04-01 13:10:37 +0200

    funka, må bare legge inn testa å finskrive litt

--- page-replacement-source/src/main/java/no/hials/page/replacement/OptimalReplacement.java
+++ page-replacement-source/src/main/java/no/hials/page/replacement/OptimalReplacement.java
@@ -24,16 +24,16 @@ public class OptimalReplacement extends ReplacementAlgorithm {
 
        // List<Integer> notLoadedPages;
         int replacements = 0; // How many page replacements made
-        int lastLoaded = 0;
         currentFrame = 0;
-
+        //Goes through all the pages
         for (int i = 0; i < pageReferences.size(); i++) {
             int page = pageReferences.get(i);
-
+            //Checks if the page is allready loaded in one of the frames
             if (!isLoaded(page)) {
-
+                //Tests which one of the frames who has a page which is not uesd 
+                //for the longest time
                 test(pageReferences, page, i);
-
+                //Replaces one of the pages with the new page
                 if (pageIn(currentFrame, page)) {
                     replacements++;
                 }
@@ -43,41 +43,50 @@ public class OptimalReplacement extends ReplacementAlgorithm {
         return replacements;
     }
 
+    
+    /**
+    * @param pageReferences The list of incoming pages
+    * @param page The current page which is not loaded
+    * @param current The current position in the list
+    */
     public void test(List<Integer> pageReferences, int page, int current) {
 
         int longestAway = 0;
+        //Checks if some of the frames are empty
         for (int i = 0; i < frames.length; i++) {
             if (frames[i] == -1) {
                 currentFrame = i;
                 return;
             }
         }
-
+        //If all frames are used
         for (int i = 0; i < frames.length; i++) {
-            boolean nextFound = false;
+     
             for (int x = current; x < pageReferences.size(); x++) {
 
                 int tempPage = pageReferences.get(x);
 
                 if (frames[i] == tempPage) {
-                    nextFound = true;
-                    if (x >= longestAway) {
+                  
+                    if (x > longestAway) {
                         longestAway = x;
                         currentFrame = i;
+                        
                     }
+                    break;
                 } else {
+                    //If the page in the current fram is not used later, then
+                    //this is the frame which gets switched out
                     if(x == (pageReferences.size() - 1)) {
                         currentFrame = i;
                         return;
                     } 
                 }
             }
-            if (nextFound) {
-                break;
-            }
+  
         }
 
     }
 }
-    // TODO - create any helper methods here if you need any
+
 
